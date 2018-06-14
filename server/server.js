66714599d13889df579feb5c70860476e31aeeef
RamirezJosue <xdemobtkx@gmail.com>
2018-06-13 20:24:45 -0500

    habilitando CORS

--- server/server.js
+++ server/server.js
@@ -6,6 +6,13 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+    next();
+});
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
